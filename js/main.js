a1a39690b010803e2c74a5a4af8b7b21ba365af9
thais maciel <thaismaciielf@hotmail.com>
2025-05-03 19:18:12 -0300

    Commitando protótipo visual da aplicação

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('#carousel-images').slick({
+        autoplay:true
+    });
+})
+
+$(document).ready(function(){
+    $('#carousel-images').slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false
+    });
+});
